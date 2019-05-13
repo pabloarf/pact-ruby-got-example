2f5a26bea812df388620fd96e9c3bc26e5d026c1
Pablo Ravera <pravera@theappraisallane.com>
2019-05-13 18:24:31 -0300

    Refactors consumer test

--- consumer/spec/got_consumer_spec.rb
+++ consumer/spec/got_consumer_spec.rb
@@ -3,41 +3,26 @@ require 'pact/consumer/rspec'
 require_relative 'pact_helper'
 
 describe 'Got Consumer', pact: true do
-  it 'devuelve personajes' do
+  before do
+    # Interaccion
     got_provider_service
-      .given('existe una lista de personajes')
-      .upon_receiving('una solicitud para obtener personajes')
-      .with(
-        method: :get,
-        path: '/personajes',
-        headers: {'Accept' => 'application/json'}
-      )
-      .will_respond_with(
-        status: 200,
-        headers: { 'Content-Type' => Pact.term(generate: 'application/json', matcher: %r{^application\/json(;\s*charset=[\w-]+)?$}) },
-        body: {
-          personajes: [
-            {
-              nombre: Pact.like('Arya Stark'),
-              edad: Pact.like(18),
-              casa: Pact.like('Stark'),
-              actor: Pact.like('Maisie Williams')
-            },
-            {
-              nombre: Pact.like('Daenerys Targaryen'),
-              edad: Pact.like(23),
-              casa: Pact.like('Targaryen'),
-              actor: Pact.like('Emilia Clarke')
-            },
-            {
-              nombre: Pact.like('Tyrion Lannister'),
-              edad: Pact.like(38),
-              casa: Pact.like('Lannister'),
-              actor: Pact.like('Peter Dinklage')
+        .given('existe una lista de personajes') # Precondicion
+        .upon_receiving('una solicitud para obtener personajes') # Solicitud requerida
+        .with(
+            method: :get,
+            path: '/personajes',
+            headers: {'Accept' => 'application/json'}
+        )
+        .will_respond_with( # Respuesta esperada
+            status: 200,
+            headers: { 'Content-Type' => Pact.term(generate: 'application/json', matcher: %r{^application\/json(;\s*charset=[\w-]+)?$}) },
+            body: {
+                personajes: Pact.each_like(nombre: Pact.like('Arya Stark'), edad: Pact.like(18), casa: Pact.like('Stark'), actor: Pact.like('Maisie Williams'))
             }
-          ]
-        }
-      )
+        )
+  end
+
+  it 'devuelve personajes' do
     # Solicitud al endpoint de nuestro provider
     got_respuesta = Faraday.get(got_provider_service.mock_service_base_url + '/personajes', nil, {'Accept' => 'application/json'})
 
